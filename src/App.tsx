572137fbfa3f40d83fcf0dc3029c3c546470c6f7
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-06 15:02:45 -0700

    WIP. add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
-
 export function App() {
     return (
-        <div className="">
-            11
+        <div className="h-screen">
+            <div className="h-full bg-yellow-500 grid place-content-center">
+                <div className="min-w-[400px] min-h-[540px] bg-yellow-700">
+                    11
+                </div>
+            </div>
         </div>
     );
 }

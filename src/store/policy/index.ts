474ab6d38c1a2662bc6b1ddfec4e5d1305a6365c
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-07 23:39:56 -0700

    WIP. mani. add policy definitions

---
+++ src/store/policy/index.ts
@@ -0,0 +1,12 @@
+export type {
+    Policy,
+} from './poly-types';
+
+export {
+    ConstrainSet,
+    ConstrainPsw,
+    UseAs,
+    namesConstrainSet,
+    namesConstrainPsw,
+    namesUseAs,
+} from './poly-types';

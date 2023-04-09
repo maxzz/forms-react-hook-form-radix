b39f5941d697f122f169db3765b59349553274a2
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-08 18:37:51 -0700

    WIP. ui. add dark/light switch. w/ icons

--- src/components/Section1_Header/index.tsx
+++ src/components/Section1_Header/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { DarkLightSwitch } from "./DarkLight";
 
 const checkboxClasses = `
 form-checkbox 
@@ -8,22 +9,26 @@ rounded border-none cursor-pointer transition-all
 
 export function Section1_Header() {
     const [isDark, setIsDark] = useState(false);
+
+    function changeMode() {
+        const body = document.getElementsByTagName('body')[0];
+        body.classList.toggle('dark');
+        setIsDark(body.classList.contains('dark'));
+    }
+
     return (
         <div className="px-4 py-3 flex items-center justify-between">
             <div className="">logo</div>
 
             {/* Darlk mode switch */}
-            <label
-                className="flex items-center space-x-2"
-                onClick={() => {
-                    const body = document.getElementsByTagName('body')[0];
-                    body.classList.toggle('dark');
-                    setIsDark(body.classList.contains('dark'));
-                }}
-            >
-                <input className={checkboxClasses} type="checkbox" />
-                <div className="">{isDark?'set light':'set dark'}</div>
-            </label>
+            {/* <label className="flex items-center space-x-2">
+                <input className={checkboxClasses} type="checkbox" onClick={changeMode} />
+                <div className="select-none">{isDark ? 'set light' : 'set dark'}</div>
+            </label> */}
+
+            <button className="p-1" title={isDark ? 'Set light mode' : 'Set dark mode'} onClick={changeMode}>
+                <DarkLightSwitch isDark={!isDark} />
+            </button>
         </div>
     );
 }

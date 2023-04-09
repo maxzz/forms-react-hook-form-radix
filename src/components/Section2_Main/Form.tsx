90106646a4c29f279cd8b16d5c5a8e80a00f3c14
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-09 15:04:38 -0700

    WIP. form. add InputWithController component

--- src/components/Section2_Main/Form.tsx
+++ src/components/Section2_Main/Form.tsx
@@ -1,19 +1,19 @@
 import React, { AllHTMLAttributes, HTMLAttributes, useEffect, useState } from 'react';
 import { Button } from './Controls';
-import { Control, useController, useForm } from 'react-hook-form';
+import { Control, Controller, UseControllerProps, useController, useForm } from 'react-hook-form';
+import { classNames } from '@/utils';
 
 type FormValues = {
     test: number;
+    inputState: string;
 };
 
 const inputClasses = "px-2 py-1 w-full rounded-sm bg-primary-200 dark:bg-primary-800";
 const turnOffAutoComplete: AllHTMLAttributes<HTMLElement> = { autoComplete: "new-password", list: "autocompleteOff", spellCheck: "false", };
 
-function Input({ control }: { control: Control<FormValues>; }) {
-    const { field } = useController({
-        control,
-        name: "test"
-    });
+function InputWithOwnState({ control }: { control: Control<FormValues>; }) {
+    const { field } = useController({ control, name: "test", });
+
     const [value, setValue] = useState(String(field.value));
 
     useEffect(() => {
@@ -33,27 +33,62 @@ function Input({ control }: { control: Control<FormValues>; }) {
 
             className={inputClasses}
 
-            //https://developer.mozilla.org/en-US/docs/Web/Security/Securing_your_site/Turning_off_form_autocompletion#preventing_autofilling_with_autocompletenew-password
-            //autoComplete="new-password" list="autocompleteOff" spellCheck="false"
             {...turnOffAutoComplete}
-        // autoComplete="off" list="autocompleteOff" spellCheck="false"
         />
     );
 }
 
-export function Form() {
+// function InputWithController({ control }: { control: Control<FormValues>; }) {
+//     return (
+//     );
+// }
 
+function InputWithController(props: UseControllerProps<FormValues>) {
+    const { field, fieldState } = useController(props);
+    return (
+        <div>
+            <input {...field} placeholder={props.name} {...turnOffAutoComplete} />
+            <div className="flex text-xs space-x-2">
+                <div className="">{fieldState.isTouched && "Touched"}</div>
+                <div className={classNames(fieldState.isDirty && 'text-yellow-500')}>{fieldState.isDirty && "Dirty"}</div>
+                <div className={classNames(fieldState.invalid && 'text-red-500')}>{fieldState.invalid ? "invalid" : "valid"}</div>
+                <div className="">{field.value}</div>
+            </div>
+        </div>
+    );
+}
+
+// function InputWithController({ control }: { control: Control<FormValues>; }) {
+//     return (
+//         <Controller
+//             name="inputState"
+//             control={control}
+//             render={({field}) => <input placeholder="inputState" {...field} />}
+//             rules={{
+//                 validate: () => {
+//                     return getValues("firstName") === "bill";
+//                 }
+//             }}
+//             defaultValue=""
+//         />
+//     );
+// }
+
+export function Form() {
     const { control, handleSubmit, reset } = useForm<FormValues>({
         defaultValues: {
-            test: 0
+            test: 0,
+            inputState: 'now'
         }
     });
+
     function onSubmit(data: FormValues) {
         return console.log('submit data', data);
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        // <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
             <div className="min-w-[400px] min-h-[540px] text-sm bg-primary-300 dark:bg-primary-900 grid grid-rows-[auto,1fr,auto]">
                 {/* Caption */}
                 <div className="px-2 py-4 bg-primary-950">
@@ -62,16 +97,18 @@ export function Form() {
 
                 {/* Body */}
                 <div className="p-4 flex flex-col space-y-4">
-                    <Input control={control} />
+                    <InputWithOwnState control={control} />
+                    <InputWithController name="inputState" control={control} rules={{ required: true }} />
+
                     <input className={inputClasses} {...turnOffAutoComplete} />
-                    <input className={inputClasses} autoComplete="off" list="autocompleteOff" spellCheck="false" />
+                    <input className={inputClasses} {...turnOffAutoComplete} />
                 </div>
 
                 {/* Buttons */}
                 <div className="p-4 border-primary-500/50 border-t flex items-center justify-end">
-                    <Button>OK</Button>
+                    <Button onClick={handleSubmit(onSubmit)}>OK</Button>
                 </div>
             </div>
-        </form>
+        </div>
     );
 }

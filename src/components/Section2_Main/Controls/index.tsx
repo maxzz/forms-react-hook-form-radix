387325b651cf7db5ed2f2abc97f2d8a87ecf157b
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-08 19:00:59 -0700

    WIP. ui. ---------- add local storage support

--- src/components/Section2_Main/Controls/index.tsx
+++ src/components/Section2_Main/Controls/index.tsx
@@ -5,7 +5,7 @@ export function Button({ className, children, ...rest }: HTMLAttributes<HTMLButt
     return (
         <button
             className={classNames(
-                "px-2 py-1 min-w-[8ch] text-primary-900 bg-primary-400 border-primary-500/50 border rounded shadow active:scale-y-95",
+                "px-2 py-1 min-w-[8ch] text-primary-900 bg-primary-400 dark:bg-primary-950 border-primary-500/50 border rounded shadow active:scale-y-95",
                 className
             )}
             {...rest}
---
+++ src/store-ui/index.ts
@@ -0,0 +1,34 @@
+import { mergeDefaultAndLoaded } from "@/utils";
+import { proxy, subscribe } from "valtio";
+export { useSnapshot } from 'valtio';
+
+const STORAGE_KEY = 'forms-react-hook-form-radix-ui';
+const STORAGE_VER = 'v1';
+
+type AppUi = {
+    dark: boolean;
+};
+
+const initialAppUi: AppUi = {
+    dark: false,
+};
+
+export const appUi = proxy<AppUi>(loadStorageAppUi());
+
+// Local storage
+
+function loadStorageAppUi(): AppUi {
+    const storage = localStorage.getItem(STORAGE_KEY);
+    if (storage) {
+        try {
+            const state = mergeDefaultAndLoaded(JSON.parse(storage)?.[STORAGE_VER], initialAppUi);
+            return state;
+        } catch (error) {
+        }
+    }
+    return initialAppUi;
+}
+
+subscribe(appUi, () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ [STORAGE_VER]: appUi }));
+});

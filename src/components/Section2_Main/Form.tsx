fd7cd45cb8dbc699f83a476d833348fcdd13365a
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-09 15:42:12 -0700

    WIP. ui. update main section dark mode

--- src/components/Section2_Main/Form.tsx
+++ src/components/Section2_Main/Form.tsx
@@ -47,12 +47,12 @@ function InputWithController(props: UseControllerProps<FormValues>) {
     const { field, fieldState } = useController(props);
     return (
         <div>
-            <input {...field} placeholder={props.name} {...turnOffAutoComplete} />
-            <div className="flex text-xs space-x-2">
-                <div className="">{fieldState.isTouched && "Touched"}</div>
-                <div className={classNames(fieldState.isDirty && 'text-yellow-500')}>{fieldState.isDirty && "Dirty"}</div>
-                <div className={classNames(fieldState.invalid && 'text-red-500')}>{fieldState.invalid ? "invalid" : "valid"}</div>
-                <div className="">{field.value}</div>
+            <input {...field} placeholder={props.name} {...turnOffAutoComplete} className={inputClasses} />
+            <div className="flex text-[.65rem] space-x-2">
+                <div className="">{fieldState.isTouched && "touched"}</div>
+                <div className={classNames(fieldState.isDirty && 'text-yellow-500')}>{fieldState.isDirty && "dirty"}</div>
+                <div className={classNames(fieldState.invalid && 'text-red-500')}>{fieldState.invalid ? "invalid" : "valid"}{fieldState.error && <span className=""> {fieldState.error.message}</span> }</div>
+                <div className="">value: '{field.value}'</div>
             </div>
         </div>
     );
@@ -88,17 +88,20 @@ export function Form() {
 
     return (
         // <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+        <div className="shadow dark:shadow-primary-500/20">
             <div className="min-w-[400px] min-h-[540px] text-sm bg-primary-300 dark:bg-primary-900 grid grid-rows-[auto,1fr,auto]">
                 {/* Caption */}
-                <div className="px-2 py-4 bg-primary-950">
+                <div className="px-2 py-4 text-primary-800 dark:text-primary-400 bg-primary-400 dark:bg-primary-950">
                     Form Caption
                 </div>
 
                 {/* Body */}
                 <div className="p-4 flex flex-col space-y-4">
                     <InputWithOwnState control={control} />
-                    <InputWithController name="inputState" control={control} rules={{ required: true }} />
+                    {/* <InputWithController name="inputState" control={control} rules={{ required: true }} /> */}
+                    <InputWithController name="inputState" control={control} rules={{
+                        validate: (value) => value === '1' || 'ERROR MESSAGE'
+                    }} />
 
                     <input className={inputClasses} {...turnOffAutoComplete} />
                     <input className={inputClasses} {...turnOffAutoComplete} />
--- src/components/Section2_Main/index.tsx
+++ src/components/Section2_Main/index.tsx
@@ -2,7 +2,10 @@ import { Form } from "./Form";
 
 export function Section2_Main() {
   return (
-    <div className="bg-primary-700 bg-gradient-to-b from-primary-500 to-primary-600 grid place-content-center">
+    <div className="
+    bg-primary-200 bg-gradient-to-b from-primary-400/40 to-primary-600/40 via-80% via-primary-400/40
+    dark:bg-primary-900 dark:bg-gradient-to-b dark:from-primary-700/40 dark:to-primary-800/20 dark:via-80% dark:via-primary-700/40
+    grid place-content-center">
         <Form/>
     </div>
   )
